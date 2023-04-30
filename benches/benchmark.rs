9a301602cc94fe9c6b6e10b94b3ba7fbce6bd6f3
Ash <ext0l@catgirl.ai>
2023-04-29 21:56:33 -0700

    chore: define a benchmark

---
+++ benches/benchmark.rs
@@ -0,0 +1,58 @@
+use bevy::{log::LogPlugin, prelude::*, winit::WinitPlugin};
+use criterion::{criterion_group, criterion_main, Criterion};
+use extol_sprite_layer::{LayerIndex, SpriteLayerPlugin};
+
+#[derive(Debug, Clone, Component, Hash, PartialEq, Eq)]
+enum SpriteLayer {
+    // we only need one 'layer' for the benchmark
+    Middle,
+}
+
+impl LayerIndex for SpriteLayer {
+    fn as_z_coordinate(&self) -> f32 {
+        use SpriteLayer::*;
+        match *self {
+            Middle => 1.,
+        }
+    }
+}
+
+fn setup_app() -> App {
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins
+            .build()
+            .disable::<WinitPlugin>()
+            .disable::<LogPlugin>(),
+    )
+    .add_plugin(SpriteLayerPlugin::<SpriteLayer>::default());
+    for _ in 0..10000 {
+        let sprite = Sprite {
+            custom_size: Some(Vec2::new(60.0, 60.0)),
+            ..default()
+        };
+        app.world.spawn((
+            SpriteBundle {
+                sprite,
+                transform: Transform::from_xyz(0., fastrand::f32(), 0.),
+                ..default()
+            },
+            SpriteLayer::Middle,
+        ));
+    }
+    app.setup();
+    app
+}
+
+// Unfortunately, bevy doesn't like running multiple apps with render plugins,
+// even if you drop one first, so if you want to iterate on benchmarking you
+// have to manually tweak this.
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut app = setup_app();
+    c.bench_function("create app", |b| {
+        b.iter(|| app.update());
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
