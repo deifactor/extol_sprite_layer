907e11ce79152c36389690554758a2e7e51cbfbf
Ash <ext0l@catgirl.ai>
2024-03-24 20:07:20 -0700

    chore: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -212,6 +212,9 @@ pub struct RenderZCoordinate(pub f32);
 
 #[cfg(test)]
 mod tests {
+    use bevy::ecs::system::RunSystemOnce;
+    use tap::Tap;
+
     use super::*;
 
     #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Component)]
@@ -251,6 +254,14 @@ mod tests {
         TransformBundle::from_transform(Transform::from_xyz(x, y, 0.0))
     }
 
+    fn get_z(world: &World, entity: Entity) -> f32 {
+        world
+            .get::<GlobalTransform>(entity)
+            .unwrap()
+            .translation()
+            .z
+    }
+
     #[test]
     fn simple() {
         let mut app = test_app();
@@ -265,14 +276,57 @@ mod tests {
             .id();
         app.update();
 
-        let get_z = |entity| {
+        assert!(get_z(&app.world, bottom) < get_z(&app.world, middle));
+        assert!(get_z(&app.world, middle) < get_z(&app.world, top));
+    }
+
+    fn layer_bundle(layer: Layer) -> impl Bundle {
+        (transform_at(0.0, 0.0), layer)
+    }
+
+    #[test]
+    fn inherited() {
+        let mut app = test_app();
+        let top = app.world.spawn(layer_bundle(Layer::Top)).id();
+        let child_with_layer = app
+            .world
+            .spawn(layer_bundle(Layer::Middle))
+            .set_parent(top)
+            .id();
+        let child_without_layer = app.world.spawn(transform_at(0.0, 0.0)).set_parent(top).id();
+        app.update();
+
+        assert_eq!(
+            get_z(&app.world, child_with_layer).floor(),
+            Layer::Middle.as_z_coordinate()
+        );
+        assert_eq!(
+            get_z(&app.world, child_without_layer).floor(),
+            get_z(&app.world, top).floor()
+        );
+    }
+
+    #[test]
+    fn y_sorting() {
+        let mut app = test_app();
+        for _ in 0..10 {
             app.world
-                .get::<GlobalTransform>(entity)
-                .unwrap()
-                .translation()
-                .z
-        };
-        assert!(get_z(bottom) < get_z(middle));
-        assert!(get_z(middle) < get_z(top));
+                .spawn((transform_at(0.0, fastrand::f32()), Layer::Top));
+        }
+        app.update();
+        let positions = app
+            .world
+            .run_system_once(|query: Query<&GlobalTransform>| -> Vec<Vec3> {
+                query
+                    .into_iter()
+                    .map(|transform| transform.translation())
+                    .collect()
+            });
+        let sorted_by_z = positions
+            .clone()
+            .tap_mut(|positions| positions.sort_by_key(|vec| OrderedFloat(vec.z)));
+        let sorted_by_y = positions
+            .tap_mut(|positions| positions.sort_by_key(|vec| Reverse(OrderedFloat(vec.y))));
+        assert_eq!(sorted_by_z, sorted_by_y);
     }
 }
